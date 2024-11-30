ac4227c56a6696efa5b71f500331603a5829d38a
Diogo Reis <diogo.reis@ipvc.pt>
2024-11-30 17:14:53 +0000

    new updates

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://browser-party-server.onrender.com');  // Substitua pela URL do seu servidor
+// Usa a variável de ambiente ou o valor padrão para desenvolvimento
+const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:3000');
 
 export default socket;
