c5c17a2a470a42707c90e23df49e5d73bc4703eb
Diogo Reis <diogo.reis@ipvc.pt>
2024-12-02 16:49:16 +0000

    fix .env

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // Usa a variável de ambiente ou o valor padrão para desenvolvimento
-const socket = io(process.env.REACT_APP_SOCKET_URL || 'https://browser-party-server.onrender.com');
+const socket = io(import.meta.env.VITE_REACT_APP_SOCKET_URL || 'https://browser-party-server.onrender.com');
 
 export default socket;
